f46b1ecabc3b0b270d0974a9ea55eed1de50ab0f
Brett Hodges <bhodges87@gmail.com>
2015-03-19 15:55:31 -0400

    added div tags to each article

--- Scripts/topStories.js
+++ Scripts/topStories.js
@@ -1,31 +1,42 @@
 var main = function(){
         $.getJSON("http://api.nytimes.com/svc/topstories/v1/home.json?&api-key=f2e425449a8965c26dd227193dc3d9c5:15:71605180", function (topStories) {
-        var newsStories = topStories.results,
-            abstract = "",
+        var newsStories = topStories.results;
+    
+    for(var i = 0; i < newsStories.length; i++){        
+        var abstract = "",
             imageUrl = "",
             title = "",
+            url = "",
             $newsArticle = $("<article>"),
             $imageDiv = $("<div class = \"image\"></div> "),
             $titleDiv = $("<div class = \"title\"></div>"),
-            $abstractDiv = $("<div class = \"abstract\"></div>");
+            $abstractDiv = $("<div class = \"abstract\"></div>"),
+            $linkDiv = $("<div class = \"link\"</div>");
         
-        for (var i = 0; i<newsStories.length; i++){
-            $newsArticle = $("<article>");
-            abstract = "";
-            title = "";
-            imageUrl = "";
-            console.log(newsStories[i]);
-            abstract = newsStories[i].abstract;
-            if(newsStories[i].multimedia !== ""){
-                imageUrl = newsStories[i].multimedia[0].url
-            }
+            $($newsArticle).append($titleDiv);
+            $($newsArticle).append($imageDiv);
+            $($newsArticle).append($abstractDiv);
+            $($newsArticle).append($linkDiv);
+        
             title = newsStories[i].title;
-            $("<h2>"+title+"</h2>").appendTo($titleDiv).appendTo($newsArticle);
-            $("<img src="+imageUrl+"><br>").appendTo($imageDiv).appendTo($newsArticle);
-            $("<p>"+abstract+"</p><br>").appendTo($abstractDiv).appendTo($newsArticle);
+            $("<h2>"+title+"</h2>").appendTo($titleDiv);
+        
+            if(newsStories[i].multimedia !== ""){
+                imageUrl = newsStories[i].multimedia[0].url;
+                $("<img src="+imageUrl+"><br>").appendTo($imageDiv);
+            }
+        
+            abstract = newsStories[i].abstract;
+            $("<p>"+abstract+"</p>").appendTo($abstractDiv);
+        
+            url = newsStories[i].url;
+            $("<a href="+url+">Link to article</a>").appendTo($linkDiv);
+        
             $(".topStories").append($newsArticle);
-        }
-            //$(".topStories").append($newsArticle);
+        
+            console.log(newsStories[i]);
+    }
+
     });
 };
 $(document).ready(main);
